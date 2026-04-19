fcac15eef0ae693ee007159987f13e9a842e8759
khushraj17 <khushrajrane@gmail.com>
2026-04-19 19:59:01 +0530

    add tabs

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import streamlit as st
 st.set_page_config(page_title="India Census Dashboard", page_icon=":bar_chart:", layout="wide")
 
 #------------------------------------------------------------
+#data cleaning
 
 df = pd.read_csv("dataset/india.csv")
 l = list(df['State'].unique())
@@ -19,24 +20,29 @@ total_lit = df["Literate"].sum()
 avg_litracy_rate = round((total_lit/total_pop)*100,2)
 sex_ratio = round((df['Female'].sum()/df['Male'].sum())*1000)
 
-col1, col2 ,col3, col4= st.columns(4)
-col1.metric("Total Population", f"{(total_pop)/10000000:.2f} Cr")
-col2.metric("Total Literate", f"{(total_lit)/10000000:.2f} Cr")
-col3.metric("Average Literacy Rate", f"{avg_litracy_rate}%")
-col4.metric("Sex ratio ",sex_ratio)
 
 #----------------------------------------------------------
 
+#main code
 
-st.sidebar.title("India Census Dashboard")
-analysis_type = st.sidebar.selectbox("Select Analysis Type", ['None',"Overall Analysis", "State Analysis"])
-# btn1 = st.sidebar.button("submit")
 
-if analysis_type == "Overall Analysis":
-    primary_param = st.sidebar.selectbox("Select Primary Parameter", param)
-    secondary_param = st.sidebar.selectbox("Select Secondary Parameter", param)
+st.title("India Census Dashboard")
+tab1, tab2 = st.tabs(["Overview", "Detailed Analysis"])
+
+with tab1:
+
+    col1, col2 ,col3, col4= st.columns(4)
+    col1.metric("Total Population", f"{(total_pop)/10000000:.2f} Cr")
+    col2.metric("Total Literate", f"{(total_lit)/10000000:.2f} Cr")
+    col3.metric("Literacy Rate", f"{avg_litracy_rate}%")
+    col4.metric("Sex ratio ",sex_ratio)
+
+    primary_param = st.selectbox("Select Primary Parameter", param)
+    secondary_param = st.selectbox("Select Secondary Parameter", param)
+
     if primary_param == 'None' or secondary_param == 'None':
         st.warning("Please select both primary and secondary parameters.")
+
     else:
         fig = px.scatter_map(df,
                             lat="Latitude",
@@ -50,7 +56,7 @@ if analysis_type == "Overall Analysis":
                             color_discrete_sequence=["#00FFFF"])
         st.plotly_chart(fig)
 
-elif analysis_type == "State Analysis":
+with tab2 : 
 
     state = st.sidebar.selectbox("Select State", df["State"].unique())
     primary_param = st.sidebar.selectbox("Select Primary Parameter", param)
